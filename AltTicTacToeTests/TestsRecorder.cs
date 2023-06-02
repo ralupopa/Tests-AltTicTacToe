d3b35d0c96ac95df425128921559e57dd1ff929a
ALTOM-PC63\raluca.popa <raluca.popa@altom.com>
2023-06-02 15:20:56 +0300

    test Draw result: recorder but also fixed

--- AltTicTacToeTests/TestsRecorder.cs
+++ AltTicTacToeTests/TestsRecorder.cs
@@ -7,6 +7,10 @@ public class MyTests
   public void OneTimeSetUp()
   {
     altDriver = new AltDriver();
+  }
+  [SetUp]
+  public void BeforeEachTest()
+  {
     altDriver.LoadScene("StartScene");
   }
 
@@ -17,7 +21,7 @@ public class MyTests
   }
 
   [Test]
-  public void Test()
+  public void TestPlayerXWins()
   {
     var PlayButton = altDriver.WaitForObject(By.PATH,"/Canvas/MainContainer/PlayButton", enabled: false);
     Assert.NotNull(PlayButton);
@@ -44,5 +48,36 @@ public class MyTests
 
     Assert.NotNull(WinInfo2);
     Assert.That(WinInfo2.GetText(), Is.EqualTo("Player X wins!"));
+  }
+
+  [Test]
+  public void TestDrawResult()
+  {
+    var PlayButton = altDriver.WaitForObject(By.PATH,"/Canvas/MainContainer/PlayButton", enabled: false);
+    Assert.NotNull(PlayButton);
+    PlayButton.Click();
+
+    var TextTMP = altDriver.WaitForObject(By.PATH,"/Canvas/MainContainer/Board/Slots/Slot8/Text (TMP)", enabled: false);
+    altDriver.Click(TextTMP.GetScreenPosition());
+    var TextTMP2 = altDriver.WaitForObject(By.PATH,"/Canvas/MainContainer/Board/Slots/Slot5/Text (TMP)", enabled: false);
+    altDriver.Click(TextTMP2.GetScreenPosition());
+    var TextTMP3 = altDriver.WaitForObject(By.PATH,"/Canvas/MainContainer/Board/Slots/Slot9/Text (TMP)", enabled: false);
+    altDriver.Click(TextTMP3.GetScreenPosition());
+    var TextTMP4 = altDriver.WaitForObject(By.PATH,"/Canvas/MainContainer/Board/Slots/Slot7/Text (TMP)", enabled: false);
+    altDriver.Click(TextTMP4.GetScreenPosition());
+    var TextTMP5 = altDriver.WaitForObject(By.PATH,"/Canvas/MainContainer/Board/Slots/Slot3/Text (TMP)", enabled: false);
+    altDriver.Click(TextTMP5.GetScreenPosition());
+    var TextTMP6 = altDriver.WaitForObject(By.PATH,"/Canvas/MainContainer/Board/Slots/Slot6/Text (TMP)", enabled: false);
+    altDriver.Click(TextTMP6.GetScreenPosition());
+    var TextTMP7 = altDriver.WaitForObject(By.PATH,"/Canvas/MainContainer/Board/Slots/Slot4/Text (TMP)", enabled: false);
+    altDriver.Click(TextTMP7.GetScreenPosition());
+    var TextTMP8 = altDriver.WaitForObject(By.PATH,"/Canvas/MainContainer/Board/Slots/Slot1/Text (TMP)", enabled: false);
+    altDriver.Click(TextTMP8.GetScreenPosition());
+    var TextTMP9 = altDriver.WaitForObject(By.PATH,"/Canvas/MainContainer/Board/Slots/Slot2/Text (TMP)", enabled: false);
+    altDriver.Click(TextTMP9.GetScreenPosition());
+
+    var WinInfo2 = altDriver.WaitForObject(By.PATH,"/Canvas/MainContainer/GameOverPanel/WinInfo", enabled: false);
+    Assert.NotNull(WinInfo2);
+    Assert.That(WinInfo2.GetText(), Is.EqualTo("It's a draw!"));
   }
 }
