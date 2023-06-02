af13ed60977c37f4b75beb373f477093a51be98f
ALTOM-PC63\raluca.popa <raluca.popa@altom.com>
2023-06-02 16:45:39 +0300

    Flow tests: draw and player O wins cases

--- AltTicTacToeTests/TestsFlow.cs
+++ AltTicTacToeTests/TestsFlow.cs
@@ -31,4 +31,26 @@ public class FlowTests: BaseTest
 
         helperMethods.AssertWinInfo("Player X wins!");
     }
+    [Test]
+    public void TestDrawResult()
+    {
+        Assert.NotNull(helperMethods.PlayButton);
+        helperMethods.PlayButton.Click();
+
+        string[] slots = {"8", "5", "9", "7", "3", "6", "4", "1", "2"};
+        helperMethods.FlowSlotsPlayed(slots);
+
+        helperMethods.AssertWinInfo("It's a draw!");
+    }
+    [Test]
+    public void TestPlayerOWins()
+    {
+        Assert.NotNull(helperMethods.PlayButton);
+        helperMethods.PlayButton.Click();
+
+        string[] slots = {"9", "7", "5", "1", "6", "4"};
+        helperMethods.FlowSlotsPlayed(slots);
+
+        helperMethods.AssertWinInfo("Player O wins!");
+    }
 }
